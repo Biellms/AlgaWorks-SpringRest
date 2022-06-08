8ccc15041859f6ac99a9d0d34ab4dd76d4287348
Biellms <biell.mendes8@gmail.com>
2022-06-07 23:23:36 -0300

    Att and developing Jakarta EE

--- algaworks-api/src/main/java/com/algaworks/controller/ClienteController.java
+++ algaworks-api/src/main/java/com/algaworks/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.algaworks.controller;
 
 import java.util.*;
 
+import javax.persistence.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,27 +12,14 @@ import com.algaworks.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	// Interface Jakarta
+	@PersistenceContext
+	private EntityManager manager;
+	
+	//Get All
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		
-		var c1 = new Cliente();
-		c1.setId(1L);
-		c1.setNome("Gabriel");
-		c1.setEmail("gabriel@email.com");
-		c1.setTelefone("11 2222-2222");
-		
-		var c2 = new Cliente();
-		c2.setId(2L);
-		c2.setNome("Bruna");
-		c2.setEmail("bruna@email.com");
-		c2.setTelefone("11 3333-3333");
-		
-		var c3 = new Cliente();
-		c3.setId(3L);
-		c3.setNome("Nicoly");
-		c3.setEmail("nicoly@email.com");
-		c3.setTelefone("11 4444-4444");
-		
-		return Arrays.asList(c1, c2, c3);
+		return manager.createQuery("from Cliente", Cliente.class)
+			.getResultList();
 	}
 }
--- algaworks-api/src/main/java/com/algaworks/model/Cliente.java
+++ algaworks-api/src/main/java/com/algaworks/model/Cliente.java
@@ -1,14 +1,25 @@
 package com.algaworks.model;
 
+import javax.persistence.*;
+
 import lombok.*;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // Gerar especificamente um ou mais Equals e HashCode
 @Getter
 @Setter
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include // EqualsAndHashCode especifico
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
+	
 	private String email;
+	
+	@Column(name = "phone")
 	private String telefone;
 	
 }
