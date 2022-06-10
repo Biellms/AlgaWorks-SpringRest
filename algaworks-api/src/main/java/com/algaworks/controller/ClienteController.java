23e5a31c00444f5e03f629a036acd8c5bdceccb0
Biellms <biell.mendes8@gmail.com>
2022-06-10 16:24:48 -0300

    Developing get clientes

--- algaworks-api/src/main/java/com/algaworks/controller/ClienteController.java
+++ algaworks-api/src/main/java/com/algaworks/controller/ClienteController.java
@@ -5,8 +5,8 @@ import java.util.*;
 import javax.persistence.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.algaworks.model.Cliente;
 import com.algaworks.repository.ClienteRepository;
@@ -15,13 +15,35 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor // Injecao de dependencia atraves do construtor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	private ClienteRepository clienteRepository;
 	
 	//Get All
-	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		return clienteRepository.findByNomeContaining("Gabriel");
+	@GetMapping
+	public List<Cliente> getAll() {
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/nome/{clienteNome}")
+	public List<Cliente> getByNameContaining(@PathVariable String clienteNome) {
+		return clienteRepository.findByNomeContaining(clienteNome);
+	}
+	
+	//Get ID
+	@GetMapping("/id/{clienteId}")
+	public ResponseEntity<Cliente> getId(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+				.map(ResponseEntity::ok) // .map(cliente -> ResponseEntity.ok(cliente))
+					.orElse(ResponseEntity.notFound().build());
+		
+	//	Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+	//		
+	//	if (cliente.isPresent()) {
+	//		return ResponseEntity.ok(cliente.get());
+	//	}
+	//		
+	//	return ResponseEntity.notFound().build();
 	}
 }
