bd3796fa2e911ae7d59a37aa84456bbfaf04708b
Biellms <biell.mendes8@gmail.com>
2022-06-20 23:39:42 -0300

    Add Exception Handler

---
+++ algaworks-api/src/main/java/com/algaworks/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.algaworks.exceptionhandler;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		return handleExceptionInternal(ex, "Hello World do Exception ", headers, status, request);
+	}
+}
